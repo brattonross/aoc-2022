968d2771795fb7e52bdfe35a2280ea6211558d8b
Ross Bratton <bratton.ross@gmail.com>
2022-12-10 14:53:20 +0000

    day 8 part 1

---
+++ src/08/01.ts
@@ -0,0 +1,88 @@
+import { splitNewLines } from "../shared/text.ts";
+
+function buildTrees(input: string) {
+  const lines = splitNewLines(input);
+
+  const trees: Array<Array<number>> = [];
+  for (let row = 0; row < lines.length; row++) {
+    const chars = lines[row].split("");
+    for (let col = 0; col < chars.length; col++) {
+      if (!trees.at(row)) {
+        trees[row] = [];
+      }
+      trees[row].push(Number(chars[col]));
+    }
+  }
+
+  return trees;
+}
+
+export default function part1(input: string) {
+  const trees = buildTrees(input);
+  const width = trees[0].length;
+  const height = trees.length;
+
+  const visibleState = trees.map((row, rowIndex) =>
+    row.map(
+      (_, colIndex) =>
+        rowIndex === 0 ||
+        colIndex === 0 ||
+        rowIndex === height - 1 ||
+        colIndex === width - 1
+    )
+  );
+
+  // View from left
+  for (let row = 1; row < height - 1; row++) {
+    let tallestTree = trees[row][0];
+    for (let col = 1; col < width - 1; col++) {
+      const tree = trees[row][col];
+      if (tree > tallestTree) {
+        tallestTree = tree;
+        visibleState[row][col] = true;
+      }
+    }
+  }
+
+  // View from right
+  for (let row = 1; row < height - 1; row++) {
+    let tallestTree = trees[row][width - 1];
+    for (let col = width - 2; col > 0; col--) {
+      const tree = trees[row][col];
+      if (tree > tallestTree) {
+        tallestTree = tree;
+        visibleState[row][col] = true;
+      }
+    }
+  }
+
+  // View from top
+  for (let col = 1; col < width - 1; col++) {
+    let tallestTree = trees[0][col];
+    for (let row = 1; row < height - 1; row++) {
+      const tree = trees[row][col];
+      if (tree > tallestTree) {
+        tallestTree = tree;
+        visibleState[row][col] = true;
+      }
+    }
+  }
+
+  // View from bottom
+  for (let col = 1; col < width - 1; col++) {
+    let tallestTree = trees[height - 1][col];
+    for (let row = height - 2; row > 0; row--) {
+      const tree = trees[row][col];
+      if (tree > tallestTree) {
+        tallestTree = tree;
+        visibleState[row][col] = true;
+      }
+    }
+  }
+
+  return visibleState.reduce(
+    (sum, row) =>
+      sum + row.reduce((rowSum, isVisible) => rowSum + (isVisible ? 1 : 0), 0),
+    0
+  );
+}
---
+++ src/08/01_test.ts
@@ -0,0 +1,15 @@
+import { assertEquals } from "https://deno.land/std@0.167.0/testing/asserts.ts";
+import part1 from "./01.ts";
+
+Deno.test(
+  "should return the number of trees visible from outside the grid",
+  () => {
+    const input = `30373
+25512
+65332
+33549
+35390`;
+
+    assertEquals(part1(input), 21);
+  }
+);
